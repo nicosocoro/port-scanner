d1c9c3ece5aa2ec5fd14b8a1a65046e499a786b0
Nicolas Socoro <nicolassocoro@gmail.com>
2025-08-03 19:43:25 -0300

    Args: Fix --scan definition

--- src/args_parser.py
+++ src/args_parser.py
@@ -5,7 +5,7 @@ def parse_args_to_config():
     parser = argparse.ArgumentParser(description="Basic TCP Port Scanner")
     parser.add_argument("--host", required=True, help="Target host (IP or domain)")
     parser.add_argument("--ports", required=False, help="Port range (e.g. 20-80)")
-    parser.add_argument("--scan", required=False, help="Flag to scan all 65535 ports. Ignore if --ports is provided")
+    parser.add_argument("--scan",  action="store_true", help="Flag to scan all 65535 ports. Ignore if --ports is provided")
     parser.add_argument("--ignore-ephemeral", action="store_true", help="Ignore ephemeral ports (32768-65535). Only works with --scan.")
     parser.add_argument("--timeout", required=False, default=1000, help="Timeout in milliseconds to analyze a port.")
     parser.add_argument("--parallel", action="store_true", help="Enable parallel scanning for maximum performance.")
